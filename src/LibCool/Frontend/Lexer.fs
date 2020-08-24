8ec35b198a99309dfe28a0d22fd48a021d2e1d72
mykola <naderevnudeduske@gmail.com>
2020-08-24 17:35:20 +0300

    wip: `Lexer`
    - Passes all lexing tests

--- src/LibCool/Frontend/Lexer.fs
+++ src/LibCool/Frontend/Lexer.fs
@@ -60,6 +60,19 @@ type Lexer(_source: Source, _diags: DiagnosticBag) =
     let eat_ws_crlf (): unit =
         while not (is_eof()) && Char.IsWhiteSpace(peek_char()) do
             eat_char()
+            
+            
+    let try_eat_linebreak(): HalfOpenRange =
+        if is_ahead "\r\n"
+        then
+            eat_chars 2u
+            HalfOpenRange.Of(_offset - 2u, _offset)
+        else if (let ch = peek_char() in ch = '\r' || ch = '\n')
+        then
+            eat_char()
+            HalfOpenRange.Of(_offset - 1u, _offset)
+        else
+            HalfOpenRange.Invalid
         
         
     let is_letter (ch: char): bool = Char.IsLetter(ch) || ch = '_'
@@ -202,198 +215,228 @@ type Lexer(_source: Source, _diags: DiagnosticBag) =
     
     
     let lex_next (): Token =
-        if _offset > _source.Size
-        then
-            invalidOp (sprintf "_offset [%d] is > _source.Size [%d]" _offset _source.Size)
-
-        eat_ws_crlf()
-        _token_start <- _offset
-        
-        if is_eof()
-        then
-            _offset <- _offset + 1u
-            Token.EOF
-        else
-        
-        let ch = peek_char()
-        
-        if is_letter ch
-        then
-            let sb_id = StringBuilder(ch.ToString())
-            let mutable token_complete = false
-            
-            while not token_complete do
-                eat_char()
-                
-                if is_eof()
-                then
-                    token_complete <- true
-                else
-                
-                let ch' = peek_char()
-                
-                token_complete <- not (is_letter ch' || is_digit ch')
-                if not token_complete
-                then
-                    sb_id.Append(ch'.ToString()) |> ignore
-                
-            Token.Of(id_kind (sb_id.ToString()), span())
-        else
-            
-        if is_digit ch
-        then
-            let sb_int = StringBuilder(ch.ToString())
-            let mutable token_complete = false
-            
-            while not token_complete do
-                eat_char()
-                
-                if is_eof()
-                then
-                    token_complete <- true
-                else
-                
-                let ch' = peek_char()
-                
-                token_complete <- not (is_digit ch')
-                if not token_complete
-                then
-                    sb_int.Append(ch'.ToString()) |> ignore
-                    
-            let mutable value = 0
-            if Int32.TryParse(sb_int.ToString(), &value)
-            then 
-                Token.Of(TokenKind.IntLiteral value, span())
-            else
-                
-            _diags.Add(Diagnostic.Of(DiagnosticSeverity.Error, "Invalid integer literal", span()))
-            Token.Invalid(span())
-        else
-           
-        match try_punctuator ch with
-        | Some punctuator_token ->
-            punctuator_token
-        | None ->
-            
-        if ch = '"'
-        then
-            // Lookahead isn't, strictly speaking, necessary to lex triple-quoted string literals.
-            // But looking ahead makes the code much, much simpler.
-            if is_ahead "\"\"\""
+        let mutable result: Token option = None
+        while Option.isNone result do
+            if _offset > _source.Size
             then
-                // Triple-quoted string literal
-                eat_chars 3u
-                    
-                let sb_qqq_literal = StringBuilder()
-                let mutable token_opt: Token option = None
+                invalidOp (sprintf "_offset [%d] is > _source.Size [%d]" _offset _source.Size)
+
+            eat_ws_crlf()
+            _token_start <- _offset
+            
+            result <-
+                if is_eof()
+                then
+                    _offset <- _offset + 1u
+                    Some Token.EOF
+                else
                 
-                while Option.isNone token_opt do
-                    if is_eof()
-                    then
-                        _diags.Add(Diagnostic.Of(DiagnosticSeverity.Error, "Unterminated string literal", span()))
-                        token_opt <- Some (Token.Of(TokenKind.StringLiteral (sb_qqq_literal.ToString()),
-                                                    HalfOpenRange.Of(_token_start + 3u, _offset)))
-                    else
+                let ch = peek_char()
+                
+                if is_letter ch
+                then
+                    let sb_id = StringBuilder(ch.ToString())
+                    let mutable token_complete = false
                     
+                    while not token_complete do
+                        eat_char()
+                        
+                        if is_eof()
+                        then
+                            token_complete <- true
+                        else
+                        
+                        let ch' = peek_char()
+                        
+                        token_complete <- not (is_letter ch' || is_digit ch')
+                        if not token_complete
+                        then
+                            sb_id.Append(ch'.ToString()) |> ignore
+                        
+                    Some (Token.Of(id_kind (sb_id.ToString()), span()))
+                else
+                    
+                if is_digit ch
+                then
+                    let sb_int = StringBuilder(ch.ToString())
+                    let mutable token_complete = false
+                    
+                    while not token_complete do
+                        eat_char()
+                        
+                        if is_eof()
+                        then
+                            token_complete <- true
+                        else
+                        
+                        let ch' = peek_char()
+                        
+                        token_complete <- not (is_digit ch')
+                        if not token_complete
+                        then
+                            sb_int.Append(ch'.ToString()) |> ignore
+                            
+                    let mutable value = 0
+                    if Int32.TryParse(sb_int.ToString(), &value)
+                    then 
+                        Some (Token.Of(TokenKind.IntLiteral value, span()))
+                    else
+                        
+                    _diags.Add(Diagnostic.Of(DiagnosticSeverity.Error, "Invalid integer literal", span()))
+                    Some (Token.Invalid(span()))
+                else
+                
+                if ch = '/'
+                then
+                    eat_char()
+                    if not (is_eof())
+                    then
+                        let ch1 = peek_char()
+                        
+                        if ch1 = '/'
+                        then
+                            // line comment
+                            eat_char()
+                            while not (is_eof()) && (try_eat_linebreak() = HalfOpenRange.Invalid) do
+                                eat_char()
+                            
+                            None
+                        else
+                        
+                        if ch1 = '*'
+                        then
+                            // multiline comment
+                            eat_char()
+                            let mutable is_comment_terminated = false
+                            while not (is_eof()) && not is_comment_terminated do
+                                is_comment_terminated <- 
+                                    peek_char() = '*' &&
+                                        (eat_char()
+                                         not (is_eof()) && peek_char() = '/')
+                                eat_char()
+                                
+                            None
+                        else
+                            
+                        Some (Token.Of(TokenKind.Slash, span()))
+                    else
+                        
+                    Some (Token.Of(TokenKind.Slash, span()))
+                else
+                   
+                match try_punctuator ch with
+                | Some punctuator_token ->
+                    Some punctuator_token
+                | None ->
+                    
+                if ch = '"'
+                then
+                    // Lookahead isn't, strictly speaking, necessary to lex triple-quoted string literals.
+                    // But looking ahead makes the code much, much simpler.
                     if is_ahead "\"\"\""
                     then
-                        token_opt <- Some (Token.Of(TokenKind.TripleQuotedStringLiteral (sb_qqq_literal.ToString()),
-                                                    HalfOpenRange.Of(_token_start + 3u, _offset + 1u)))
+                        // Triple-quoted string literal
                         eat_chars 3u
-                    else
+                            
+                        let sb_qqq_literal = StringBuilder()
+                        let mutable token_opt: Token option = None
                         
-                    sb_qqq_literal.Append(peek_char()) |> ignore
-                    eat_char()
-                
-                token_opt.Value        
-            else
-
-            eat_char()
-            
-            let sb_literal = StringBuilder()
-            let mutable token_opt: Token option = None
-
-            while Option.isNone token_opt do
-                if is_eof()
-                then
-                    _diags.Add(Diagnostic.Of(DiagnosticSeverity.Error, "Unterminated string literal", span()))
-                    token_opt <- Some (Token.Of(TokenKind.StringLiteral (sb_literal.ToString()),
-                                                HalfOpenRange.Of(_token_start + 1u, _offset)))
-                else
-
-                let ch1 = peek_char()
-                if ch1 = '"'
-                then
-                    token_opt <- Some (Token.Of(TokenKind.StringLiteral (sb_literal.ToString()),
-                                                HalfOpenRange.Of(_token_start + 1u, _offset + 1u)))
-                    eat_char()
-                else
-                    
-                if ch1 = '\\'
-                then
-                    eat_char()
-                    // We treat code similar to the following `\ \r\n`
-                    // as an escaped line-break, in spite of the fact
-                    // there's a space between `\` and `\r\n`.
-                    // This is consistent with real compilers, AFAIK.
-                    eat_ws()
-                    
-                    if is_eof()
-                    then
-                        _diags.Add(Diagnostic.Of(DiagnosticSeverity.Error, "Unterminated string literal", span()))
-                        token_opt <- Some (Token.Of(TokenKind.StringLiteral (sb_literal.ToString()),
-                                                    HalfOpenRange.Of(_token_start + 1u, _offset)))
+                        while Option.isNone token_opt do
+                            if is_eof()
+                            then
+                                _diags.Add(Diagnostic.Of(DiagnosticSeverity.Error, "Unterminated string literal", span()))
+                                token_opt <- Some (Token.Of(TokenKind.StringLiteral (sb_qqq_literal.ToString()),
+                                                            HalfOpenRange.Of(_token_start + 3u, _offset)))
+                            else
+                            
+                            if is_ahead "\"\"\""
+                            then
+                                token_opt <- Some (Token.Of(TokenKind.TripleQuotedStringLiteral (sb_qqq_literal.ToString()),
+                                                            HalfOpenRange.Of(_token_start + 3u, _offset + 1u)))
+                                eat_chars 3u
+                            else
+                                
+                            sb_qqq_literal.Append(peek_char()) |> ignore
+                            eat_char()
+                        
+                        token_opt        
                     else
 
-                    let ch2 = peek_char()
-                    let is_line_break = is_ahead "\r\n" || ch2 = '\r' || ch2 = '\n'
+                    eat_char()
+                    
+                    let sb_literal = StringBuilder()
+                    let mutable token_opt: Token option = None
 
-                    // If a line-break is ahead of us, we do nothing.
-                    // A dedicated branch outside of the escaped chars processing code will deal with it.                         
-                    if not is_line_break
-                    then
-                        eat_char()
-                        
-                        if _escaped_char_map.ContainsKey(ch2)
+                    while Option.isNone token_opt do
+                        if is_eof()
                         then
-                            sb_literal.Append(_escaped_char_map.[ch2]) |> ignore
+                            _diags.Add(Diagnostic.Of(DiagnosticSeverity.Error, "Unterminated string literal", span()))
+                            token_opt <- Some (Token.Of(TokenKind.StringLiteral (sb_literal.ToString()),
+                                                        HalfOpenRange.Of(_token_start + 1u, _offset)))
                         else
+
+                        let ch1 = peek_char()
+                        if ch1 = '"'
+                        then
+                            token_opt <- Some (Token.Of(TokenKind.StringLiteral (sb_literal.ToString()),
+                                                        HalfOpenRange.Of(_token_start + 1u, _offset + 1u)))
+                            eat_char()
+                        else
+                            
+                        if ch1 = '\\'
+                        then
+                            eat_char()
+                            // We treat code similar to the following `\ \r\n`
+                            // as an escaped line-break, in spite of the fact
+                            // there's a space between `\` and `\r\n`.
+                            // This is consistent with real compilers, AFAIK.
+                            eat_ws()
+                            
+                            if is_eof()
+                            then
+                                _diags.Add(Diagnostic.Of(DiagnosticSeverity.Error, "Unterminated string literal", span()))
+                                token_opt <- Some (Token.Of(TokenKind.StringLiteral (sb_literal.ToString()),
+                                                            HalfOpenRange.Of(_token_start + 1u, _offset)))
+                            else
+
+                            let ch2 = peek_char()
+                            let is_linebreak = is_ahead "\r\n" || ch2 = '\r' || ch2 = '\n'
+
+                            // If a line-break is ahead of us, we do nothing.
+                            // A dedicated branch outside of the escaped chars processing code will deal with it.                         
+                            if not is_linebreak
+                            then
+                                eat_char()
+                                
+                                if _escaped_char_map.ContainsKey(ch2)
+                                then
+                                    sb_literal.Append(_escaped_char_map.[ch2]) |> ignore
+                                else
+                                    _diags.Add(Diagnostic.Of(DiagnosticSeverity.Error,
+                                                             "Invalid escaped char in the string literal",
+                                                             HalfOpenRange.Of(_offset - 2u, _offset)))
+                        else
+                            
+                        let linebreak_span = try_eat_linebreak()
+                        if linebreak_span <> HalfOpenRange.Invalid
+                        then
                             _diags.Add(Diagnostic.Of(DiagnosticSeverity.Error,
-                                                     "Invalid escaped char in the string literal",
-                                                     HalfOpenRange.Of(_offset - 2u, _offset)))
+                                                     "String literals cannot contain line breaks",
+                                                     linebreak_span))
+                        else
+
+                        sb_literal.Append(ch1) |> ignore
+                        eat_char()
+                        
+                    token_opt        
                 else
                     
-                let line_break_span =
-                    if is_ahead "\r\n"
-                    then
-                        eat_chars 2u
-                        HalfOpenRange.Of(_offset - 2u, _offset)
-                    else if ch1 = '\r' || ch1 = '\n'
-                    then
-                        eat_char()
-                        HalfOpenRange.Of(_offset - 1u, _offset)
-                    else
-                        HalfOpenRange.Invalid
-
-                if line_break_span <> HalfOpenRange.Invalid
-                then
-                    _diags.Add(Diagnostic.Of(DiagnosticSeverity.Error,
-                                             "String literals cannot contain line breaks",
-                                             line_break_span))
-                else
-
-                sb_literal.Append(ch1) |> ignore
                 eat_char()
-                
-            token_opt.Value        
-        else
-            
-        _diags.Add(Diagnostic.Of(DiagnosticSeverity.Error, sprintf "Unexpected character '%c'" ch, span()))
-        let token = Token.Invalid(span())
-        
-        eat_char()
 
-        token
+                _diags.Add(Diagnostic.Of(DiagnosticSeverity.Error, sprintf "Unexpected character '%c'" ch, span()))
+                Some (Token.Invalid(span()))
+            
+        result.Value
     
     
     member _.LexNext(): Token = lex_next ()
