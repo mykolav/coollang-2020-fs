19758f71a34174296ccb1ac6648e432c06203746
mykola <naderevnudeduske@gmail.com>
2020-10-31 11:29:38 +0200

    Inline a redundant class

--- src/LibCool/TranslatorParts/Translator.fs
+++ src/LibCool/TranslatorParts/Translator.fs
@@ -1,6 +1,7 @@
 namespace rec LibCool.TranslatorParts
 
 
+open System.Collections.Generic
 open LibCool.SourceParts
 open LibCool.DiagnosticParts
 open LibCool.AstParts
@@ -11,8 +12,32 @@ open LibCool.TranslatorParts
 type Translator private () =
         
     
+    static let collect_class_nodes (program_syntax: ProgramSyntax)
+                                   (diags: DiagnosticBag)
+                                   (source: Source)
+                                   : IReadOnlyDictionary<TYPENAME, AstNode<ClassSyntax>> =
+        let map = Dictionary<TYPENAME, AstNode<ClassSyntax>>()
+        
+        let add_class_syntax (class_node: AstNode<ClassSyntax>): unit =
+            let class_syntax = class_node.Syntax
+            if map.ContainsKey(class_syntax.NAME.Syntax)
+            then
+                let prev_class_syntax = map.[class_syntax.NAME.Syntax].Syntax
+                let message = sprintf "The program already contains a class '%O' at %O"
+                                      class_syntax.NAME.Syntax
+                                      (source.Map(prev_class_syntax.NAME.Span.First))
+                                      
+                diags.Error(message, class_syntax.NAME.Span)
+            else
+                map.Add(class_syntax.NAME.Syntax, class_node)
+        
+        program_syntax.Classes |> Seq.iter add_class_syntax
+        
+        map :> IReadOnlyDictionary<_, _>
+    
+    
     static member Translate(program_syntax: ProgramSyntax, diags: DiagnosticBag, source: Source): string =
-        let class_node_map = ClassDeclCollector(program_syntax, diags, source).Collect()
+        let class_node_map = collect_class_nodes program_syntax diags source
         if diags.ErrorsCount <> 0
         then
             ""
