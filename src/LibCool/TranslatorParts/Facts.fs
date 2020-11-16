78f51748ad586103a9e57d469832ce98fbeb151d
mykola <naderevnudeduske@gmail.com>
2020-11-16 02:00:59 +0200

    wip: Minimum viable Windows runtime.

---
+++ src/LibCool/TranslatorParts/Facts.fs
@@ -0,0 +1,7 @@
+namespace LibCool.TranslatorParts
+
+
+module SysVAmd64AbiFacts =
+    let ActualRegs = [| "%rdi"; "%rsi"; "%rdx"; "%rcx"; "%r8"; "%r9" |]
+    let CalleeSavedRegs = [| "%rbx"; "%r12"; "%r13"; "%r14"; "%r15" |]
+    let CallerSavedRegs = [| "%r10", "%r11" |]
