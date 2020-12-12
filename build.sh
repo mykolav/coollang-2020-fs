9884ad2c33cc8b4d186b490de4bfd02c6f85df6a
mykola <naderevnudeduske@gmail.com>
2020-12-12 18:41:59 +0200

    Add a primitive build script

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+YELLOW='\033[1;33m'
+NC='\033[0m' # No Color
+
+echo -e "${YELLOW}Building F#${NC}\n" && \
+dotnet build fs-coollang.sln && \
+echo -e "\n${YELLOW}Assembling the runtime${NC}\n" && \
+as -o ./src/Runtime/rt_linux.o ./src/Runtime/rt_linux.s && \
+as -o ./src/Runtime/rt_windows.o ./src/Runtime/rt_windows.s && \
+as -o ./src/Runtime/rt_common.o ./src/Runtime/rt_common.s && \
+echo -e "${YELLOW}Done${NC}\n"
