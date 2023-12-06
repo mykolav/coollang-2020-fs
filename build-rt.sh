71c8e1fc7218bf6d0c8eec16eb8f22d2a589b375
mykolav <naderevnudeduske@gmail.com>
2023-12-06 19:22:16 +0200

    Add rt_memory.s to build-rt.sh

--- build-rt.sh
+++ build-rt.sh
@@ -2,4 +2,5 @@
 
 as -o ./src/Runtime/rt_linux.o ./src/Runtime/rt_linux.s && \
 as -o ./src/Runtime/rt_windows.o ./src/Runtime/rt_windows.s && \
+as -o ./src/Runtime/rt_memory.o ./src/Runtime/rt_memory.s && \
 as -o ./src/Runtime/rt_common.o ./src/Runtime/rt_common.s
