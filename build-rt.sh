28a5d44c6af260572130f4034701cc54f672df57
mykolav <naderevnudeduske@gmail.com>
2023-12-20 12:29:16 +0200

    Extract rt_runtime.s from rt_common.s

--- build-rt.sh
+++ build-rt.sh
@@ -5,5 +5,6 @@ as -o ./rt_linux.o ./rt_linux.s && \
 as -o ./rt_windows.o ./rt_windows.s && \
 as -o ./rt_gen_gc.o ./rt_gen_gc.s && \
 as -o ./rt_memory.o ./rt_memory.s && \
+as -o ./rt_runtime.o ./rt_runtime.s && \
 as -o ./rt_common.o ./rt_common.s && \
 popd
--- src/LibCool/DriverParts/EmitExeHandler.fs
+++ src/LibCool/DriverParts/EmitExeHandler.fs
@@ -92,12 +92,14 @@ type EmitExeHandler(_writer: IWriteLine) =
     static member Link(obj_file: string, exe_file: string): string[] =
         let rt_dir = EmitExeHandler.ResolveRtDir()
         let rt_common_path = Path.Combine(rt_dir, "rt_common.o")
+        let rt_runtime_path = Path.Combine(rt_dir, "rt_runtime.o")
         let rt_gen_gc_path = Path.Combine(rt_dir, "rt_gen_gc.o")
         let rt_memory_path = Path.Combine(rt_dir, "rt_memory.o")
         let ld_args = StringBuilder(
             $"-o %s{exe_file} " +
             $"-e main %s{obj_file} " +
             $"\"%s{rt_common_path}\" " +
+            $"\"%s{rt_runtime_path}\" " +
             $"\"%s{rt_memory_path}\" " +
             $"\"%s{rt_gen_gc_path}\" ")
 
