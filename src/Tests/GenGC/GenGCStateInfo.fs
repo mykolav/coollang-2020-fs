b2608eb81a478267285b87cfc193406e0d2f9e40
mykolav <naderevnudeduske@gmail.com>
2024-01-06 23:17:27 +0200

    Add the F# code for a few more `GenGC` test cases.

--- src/Tests/GenGC/GenGCStateInfo.fs
+++ src/Tests/GenGC/GenGCStateInfo.fs
@@ -39,38 +39,66 @@ GenGC: ALLOC PTR   = 1950022516808
 GenGC: ALLOC LIMIT = 1950022533120
 *)
 [<IsReadOnly; Struct>]
-type GenGCStateInfo =
+type HeapInfo =
     { HeapStart: int64
       HeapEnd: int64
+      // GenGC's heap header
       L0: int64
       L1: int64
       L2: int64
       L3: int64
-      L4: int64
-      Minor0: int64
-      Minor1: int64
-      Major0: int64
-      Major1: int64
-      StackBase: int64
-      AllocPtr: int64
-      AllocLimit: int64 }
+      L4: int64 }
 
 
-    static member Empty: GenGCStateInfo =
-        { GenGCStateInfo.HeapStart = 0
+    static member Empty: HeapInfo =
+        { HeapInfo.HeapStart = 0
           HeapEnd = 0
           L0 = 0
           L1 = 0
           L2 = 0
           L3 = 0
-          L4 = 0
-          Minor0 = 0
+          L4 = 0 }
+
+
+[<IsReadOnly; Struct>]
+type GenGCHistory =
+    { Minor0: int64
+      Minor1: int64
+      Major0: int64
+      Major1: int64 }
+
+
+    static member Empty: GenGCHistory =
+        { Minor0 = 0
           Minor1 = 0
           Major0 = 0
-          Major1 = 0
-          StackBase = 0
-          AllocPtr = 0
+          Major1 = 0 }
+
+
+[<IsReadOnly; Struct>]
+type AllocInfo =
+    { AllocPtr: int64
+      AllocLimit: int64 }
+
+
+    static member Empty: AllocInfo =
+        { AllocPtr = 0
           AllocLimit = 0 }
+
+
+[<IsReadOnly; Struct>]
+type GenGCStateInfo =
+    { HeapInfo: HeapInfo
+      History: GenGCHistory
+      StackBase: int64
+      AllocInfo: AllocInfo }
+
+
+    static member Empty: GenGCStateInfo =
+        { HeapInfo = HeapInfo.Empty
+          History = GenGCHistory.Empty
+          StackBase = 0
+          AllocInfo = AllocInfo.Empty }
 
 
     static member Parse(lines: seq<string>): seq<GenGCStateInfo> =
@@ -81,20 +109,23 @@ type GenGCStateInfo =
         let mutable have_state_info = GenGCStateInfo.MoveToNext(lit)
         while have_state_info do
             let state_info =
-                { GenGCStateInfo.HeapStart = GenGCStateInfo.TakeInt64("GenGC: HEAP START", lit)
-                  HeapEnd = GenGCStateInfo.TakeInt64("GenGC: HEAP END", lit)
-                  L0 = GenGCStateInfo.TakeInt64("GenGC: L0", lit)
-                  L1 = GenGCStateInfo.TakeInt64("GenGC: L1", lit)
-                  L2 = GenGCStateInfo.TakeInt64("GenGC: L2", lit)
-                  L3 = GenGCStateInfo.TakeInt64("GenGC: L3", lit)
-                  L4 = GenGCStateInfo.TakeInt64("GenGC: L4", lit)
-                  Minor0 = GenGCStateInfo.TakeInt64("GenGC: MINOR0", lit)
-                  Minor1 = GenGCStateInfo.TakeInt64("GenGC: MINOR1", lit)
-                  Major0 = GenGCStateInfo.TakeInt64("GenGC: MAJOR0", lit)
-                  Major1 = GenGCStateInfo.TakeInt64("GenGC: MAJOR1", lit)
+                { GenGCStateInfo.HeapInfo =
+                    { HeapInfo.HeapStart = GenGCStateInfo.TakeInt64("GenGC: HEAP START", lit)
+                      HeapEnd = GenGCStateInfo.TakeInt64("GenGC: HEAP END", lit)
+                      L0 = GenGCStateInfo.TakeInt64("GenGC: L0", lit)
+                      L1 = GenGCStateInfo.TakeInt64("GenGC: L1", lit)
+                      L2 = GenGCStateInfo.TakeInt64("GenGC: L2", lit)
+                      L3 = GenGCStateInfo.TakeInt64("GenGC: L3", lit)
+                      L4 = GenGCStateInfo.TakeInt64("GenGC: L4", lit) }
+                  History =
+                      { Minor0 = GenGCStateInfo.TakeInt64("GenGC: MINOR0", lit)
+                        Minor1 = GenGCStateInfo.TakeInt64("GenGC: MINOR1", lit)
+                        Major0 = GenGCStateInfo.TakeInt64("GenGC: MAJOR0", lit)
+                        Major1 = GenGCStateInfo.TakeInt64("GenGC: MAJOR1", lit) }
                   StackBase = GenGCStateInfo.TakeInt64("GenGC: STACK BASE", lit)
-                  AllocPtr = GenGCStateInfo.TakeInt64("GenGC: ALLOC PTR", lit)
-                  AllocLimit = GenGCStateInfo.TakeInt64("GenGC: ALLOC LIMIT", lit) }
+                  AllocInfo =
+                      { AllocPtr = GenGCStateInfo.TakeInt64("GenGC: ALLOC PTR", lit)
+                        AllocLimit = GenGCStateInfo.TakeInt64("GenGC: ALLOC LIMIT", lit) } }
             state_infos.Add(state_info)
 
             have_state_info <- GenGCStateInfo.MoveToNext(lit)
--- src/Tests/GenGC/GenGCTests.fs
+++ src/Tests/GenGC/GenGCTests.fs
@@ -24,6 +24,52 @@ type GenGCTests(test_output: ITestOutputHelper) =
         Assert.Equal(expected=state_infos[0], actual=state_infos[1])
 
 
+    [<Fact>]
+    member this.``When no allocs/collections in a loop the heap state doesn't change``() =
+        // Arrange
+        // Act
+        let program_output = this.CompileAndRun("Runtime/GenGC/NoAlloc3.cool")
+        let state_infos = Array.ofSeq (GenGCStateInfo.Parse(program_output.Output))
+        // Assert
+        let expected_state_info = state_infos[0]
+        let mutable i = 1
+        while (i < state_infos.Length) do
+            Assert.Equal(expected=expected_state_info, actual=state_infos[i])
+            i <- i + 1
+
+
+    [<Fact>]
+    member this.``When no allocs collection doesn't change the heap state``() =
+        // Arrange
+        // Act
+        let program_output = this.CompileAndRun("Runtime/GenGC/NoAlloc2.cool")
+        let state_infos = Array.ofSeq (GenGCStateInfo.Parse(program_output.Output))
+        // Assert
+        // We don't expect the histories to be the same as
+        // the history naturally changes after each collection.
+        Assert.Equal(expected=state_infos[0].HeapInfo, actual=state_infos[1].HeapInfo)
+        Assert.Equal(expected=state_infos[0].StackBase, actual=state_infos[1].StackBase)
+        Assert.Equal(expected=state_infos[0].AllocInfo, actual=state_infos[1].AllocInfo)
+
+
+    [<Fact>]
+    member this.``When no allocs collection in a loop doesn't change the heap state``() =
+        // Arrange
+        // Act
+        let program_output = this.CompileAndRun("Runtime/GenGC/NoAlloc4.cool")
+        let state_infos = Array.ofSeq (GenGCStateInfo.Parse(program_output.Output))
+        // Assert
+        let expected_state_info = state_infos[0]
+        let mutable i = 1
+        while (i < state_infos.Length) do
+            // We don't expect the histories to be the same as
+            // the history naturally changes after each collection.
+            Assert.Equal(expected=expected_state_info.HeapInfo, actual=state_infos[i].HeapInfo)
+            Assert.Equal(expected=expected_state_info.StackBase, actual=state_infos[i].StackBase)
+            Assert.Equal(expected=expected_state_info.AllocInfo, actual=state_infos[i].AllocInfo)
+            i <- i + 1
+
+
     member private this.CompileAndRun(path: string): ProgramOutput =
         // Build a program's path relative to the 'CoolBuild' folder.
         let path = Path.Combine(CompilerTestCaseSource.ProgramsPath, path).Replace("\\", "/")
